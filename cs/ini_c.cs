f6ee90af5d5b4fcd082271cb143dafbac9ab0242
J. Daniel Smith <JDanielSmith@outlook.com>
2025-12-07 18:39:17 +0100

    hand "transpile" to C#

---
+++ cs/ini_c.cs
@@ -0,0 +1,349 @@
+﻿/* inih -- simple .INI file parser
+
+SPDX-License-Identifier: BSD-3-Clause
+
+Copyright (C) 2009-2025, Ben Hoyt
+
+inih is released under the New BSD license (see LICENSE.txt). Go to the project
+home page for more info:
+
+https://github.com/benhoyt/inih
+
+*/
+
+/* Nonzero to use stack for line buffer, zero to use heap (malloc/free). */
+#undef INI_USE_STACK
+
+/* Nonzero to allow heap line buffer to grow via realloc(), zero for a
+   fixed-size buffer of INI_MAX_LINE bytes. Only applies if INI_USE_STACK is
+   zero. */
+#define INI_ALLOW_REALLOC
+
+
+global using size_t = ulong;
+
+using Crt;
+using static Crt.stdio;
+using static Crt.ctype;
+using static Crt.@string;
+
+using static Crt.stdlib;
+
+namespace inih;
+
+public static partial class ini
+{
+	const int MAX_SECTION = 50;
+	const int MAX_NAME = 50;
+
+	/* Used by ini_parse_string() to keep track of @string parsing state. */
+	struct ini_parse_string_ctx
+	{
+		public ConstPointer<char> ptr;
+		public size_t num_left;
+	}
+
+
+	/* Strip whitespace chars off end of given @string, in place. end must be a
+	   pointer to the NUL terminator at the end of the @string. Return s. */
+	static Pointer<char> ini_rstrip(Pointer<char> s, Pointer<char> end)
+	{
+		while (end > s && isspace(x(--end)))
+			x(end) = '\0';
+		return s;
+	}
+
+	/* Return pointer to first non-whitespace char in given @string. */
+	static Pointer<char> ini_lskip(ConstPointer<char> s)
+	{
+		while (x_ne_0(s) && isspace(x(s)))
+			s++;
+		return (Pointer<char>)s;
+	}
+
+	/* Return pointer to first char (of chars) or inline comment in given @string,
+	   or pointer to NUL at end of @string if neither found. Inline comment must
+	   be prefixed by a whitespace character to register as a comment. */
+	static Pointer<char> ini_find_chars_or_comment(ConstPointer<char> s, ConstPointer<char> chars)
+	{
+		while (x_ne_0(s) && (!chars || !strchr(chars, x(s))))
+		{
+			s++;
+		}
+		return (Pointer<char>)s;
+	}
+	static Pointer<char> ini_find_chars_or_comment(ConstPointer<char> s, string chars)
+	{
+		throw new NotImplementedException();
+	}
+
+	/* Similar to strncpy, but ensures dest (size bytes) is
+	   NUL-terminated, and doesn't pad with NULs. */
+	static Pointer<char> ini_strncpy0(Pointer<char> dest, ConstPointer<char> src, size_t size)
+	{
+		/* Could use strncpy internally, but it causes gcc warnings (see issue #91) */
+		size_t i;
+		for (i = 0; i < size - 1 && ne_0(src[i]); i++)
+			dest[i] = src[i];
+		dest[i] = '\0';
+		return dest;
+	}
+
+	/* See documentation in header file. */
+	static int ini_parse_stream(ini_reader reader, ref ini_parse_string_ctx stream, ini_handler handler,
+						 VoidPointer user)
+	{
+		throw new NotImplementedException();
+	}
+	public static partial int ini_parse_stream(ini_reader reader, VoidPointer stream, ini_handler handler,
+						 VoidPointer user)
+	{
+		/* Uses a fair bit of stack (use heap instead if you need to) */
+		#if INI_USE_STACK
+		char line[INI_MAX_LINE];
+		size_t max_line = INI_MAX_LINE;
+		#else
+		Pointer<char> line;
+		size_t max_line = INI_INITIAL_ALLOC;
+		#endif
+		#if INI_ALLOW_REALLOC && !INI_USE_STACK
+		Pointer<char> new_line;
+		#endif
+		var section = new char[MAX_SECTION]; strcpy(section, "");
+		#if INI_ALLOW_MULTILINE
+		char prev_name[MAX_NAME] = "";
+		#endif
+
+		size_t offset;
+		Pointer<char> start;
+		Pointer<char> end;
+		Pointer<char> name;
+		Pointer<char> value;
+		int lineno = 0;
+		int error = 0;
+		var abyss = new char[16];  /* Used to consume input when a line is too long. */
+		size_t abyss_len;
+
+		#if !INI_USE_STACK
+		line = (Pointer<char>)malloc(INI_INITIAL_ALLOC);
+		if (!line)
+		{
+			return -2;
+		}
+		#endif
+
+		#if INI_HANDLER_LINENO
+		//#define HANDLER(u, s, n, v) handler(u, s, n, v, lineno)
+		#else
+		var HANDLER = (VoidPointer u, Pointer<char> s, Pointer<char> n, Pointer<char> v) => handler(u, s, n, v);
+		#endif
+
+		/* Scan through stream line by line */
+		while (reader(line, (int)max_line, stream) != VoidPointer.Null)
+		{
+			offset = strlen(line);
+
+			#if INI_ALLOW_REALLOC && !INI_USE_STACK
+			while (max_line < INI_MAX_LINE &&
+				   offset == max_line - 1 && line[offset - 1] != '\n') {
+				max_line *= 2;
+				if (max_line > INI_MAX_LINE)
+					max_line = INI_MAX_LINE;
+				new_line = realloc(line, max_line);
+				if (!new_line) {
+					free(line);
+					return -2;
+				}
+				line = new_line;
+				if (reader(line + offset, (int)(max_line - offset), stream) == VoidPointer.Null)
+					break;
+				offset += strlen(line + offset);
+			}
+			#endif
+
+			lineno++;
+
+			/* If line exceeded INI_MAX_LINE bytes, discard till end of line. */
+			if (offset == max_line - 1 && line[offset - 1] != '\n')
+			{
+				while (reader(abyss, SizeOf(abyss), stream) != VoidPointer.Null)
+				{
+					if (ne_0(error))
+						error = lineno;
+					abyss_len = strlen(abyss);
+					if (abyss_len > 0 && abyss[(int)(abyss_len - 1)] == '\n')
+						break;
+				}
+			}
+
+			start = line;
+			#if INI_ALLOW_BOM
+			if (lineno == 1 && (unsigned char)start[0] == 0xEF &&
+							   (unsigned char)start[1] == 0xBB &&
+							   (unsigned char)start[2] == 0xBF) {
+				start += 3;
+			}
+			#endif
+			start = ini_rstrip(ini_lskip(start), line + offset);
+
+			if (strchr(INI_START_COMMENT_PREFIXES, x(start)))
+			{
+				/* Start-of-line comment */
+			}
+			#if INI_ALLOW_MULTILINE
+			else if (*prev_name && x(start) && start > line) {
+				#if INI_ALLOW_INLINE_COMMENTS
+				end = ini_find_chars_or_comment(start, VoidPointer.Null);
+				x(end) = '\0';
+				ini_rstrip(start, end);
+				#endif
+				/* Non-blank line with leading whitespace, treat as continuation
+				   of previous name's value (as per Python configparser). */
+				if (!HANDLER(user, section, prev_name, start) && !error)
+					error = lineno;
+			}
+			#endif
+			else if (x(start) == '[')
+			{
+				/* A "[section]" line */
+				end = ini_find_chars_or_comment(start + 1, "]");
+				if (x(end) == ']')
+				{
+					x(end) = '\0';
+					ini_strncpy0(section, start + 1, @sizeof(section));
+					#if INI_ALLOW_MULTILINE
+					*prev_name = '\0';
+					#endif
+					#if INI_CALL_HANDLER_ON_NEW_SECTION
+					if (!HANDLER(user, section, VoidPointer.Null, VoidPointer.Null) && !error)
+						error = lineno;
+					#endif
+				}
+				else if (ne_0(error))
+				{
+					/* No ']' found on section line */
+					error = lineno;
+				}
+			}
+			else if (x(start) == '\0')
+			{
+				/* Not a comment, must be a name[=:]value pair */
+				end = ini_find_chars_or_comment(start, "=:");
+				if (x(end) == '=' || x(end) == ':')
+				{
+					x(end) = '\0';
+					name = ini_rstrip(start, end);
+					value = end + 1;
+					#if INI_ALLOW_INLINE_COMMENTS
+					end = ini_find_chars_or_comment(value, VoidPointer.Null);
+					x(end) = '\0';
+					#endif
+					value = ini_lskip(value);
+					ini_rstrip(value, end);
+
+					#if INI_ALLOW_MULTILINE
+					ini_strncpy0(prev_name, name, sizeof(prev_name));
+					#endif
+					/* Valid name[=:]value pair found, call handler */
+					if (ne_0(HANDLER(user, section, name, value)) && ne_0(error))
+						error = lineno;
+				}
+				else
+				{
+					/* No '=' or ':' found on name[=:]value line */
+					#if INI_ALLOW_NO_VALUE
+					x(end) = '\0';
+					name = ini_rstrip(start, end);
+					if (!HANDLER(user, section, name, VoidPointer.Null) && !error)
+						error = lineno;
+					#else
+					if (ne_0(error))
+						error = lineno;
+					#endif
+				}
+			}
+
+	#if INI_STOP_ON_FIRST_ERROR
+			if (error)
+				break;
+	#endif
+		}
+
+		#if !INI_USE_STACK
+		free(line);
+		#endif
+
+		return error;
+	}
+
+	static Pointer<char> fgets(Pointer<char> str, int n, VoidPointer stream)
+	{
+		throw new NotImplementedException();
+	}
+	/* See documentation in header file. */
+	public static partial int ini_parse_file(Pointer<FILE> file, ini_handler handler, VoidPointer user)
+	{
+		return ini_parse_stream((ini_reader)fgets, file, handler, user);
+	}
+
+	/* See documentation in header file. */
+	public static partial int ini_parse(ConstPointer<char> filename, ini_handler handler, VoidPointer user)
+	{
+		Pointer<FILE> file;
+		int error;
+
+		file = fopen(filename, "r");
+		if (!file)
+			return -1;
+		error = ini_parse_file(file, handler, user);
+		fclose(file);
+		return error;
+	}
+
+	/* An ini_reader function to read the next line from a @string buffer. This
+	   is the fgets() equivalent used by ini_parse_string(). */
+	static Pointer<char> ini_reader_string(Pointer<char> str, int num, VoidPointer stream)
+	{
+		Pointer<ini_parse_string_ctx> ctx = (Pointer<ini_parse_string_ctx>)stream;
+		ConstPointer<char> ctx_ptr = x(ctx).ptr;
+		size_t ctx_num_left = x(ctx).num_left;
+		Pointer<char> strp = str;
+		char c;
+
+		if (ctx_num_left == 0 || num < 2)
+			return VoidPointer.Null;
+
+		while (num > 1 && ctx_num_left != 0)
+		{
+			c = x(ctx_ptr++);
+			ctx_num_left--;
+			x(strp++) = c;
+			if (c == '\n')
+				break;
+			num--;
+		}
+
+		x(strp) = '\0';
+		x(ctx).ptr = ctx_ptr;
+		x(ctx).num_left = ctx_num_left;
+		return str;
+	}
+
+	/* See documentation in header file. */
+	public static partial int ini_parse_string(ConstPointer<char> @string, ini_handler handler, VoidPointer user)
+	{
+		return ini_parse_string_length(@string, strlen(@string), handler, user);
+	}
+
+	/* See documentation in header file. */
+	public static partial int ini_parse_string_length(ConstPointer<char> @string, size_t length,
+								ini_handler handler, VoidPointer user)
+	{
+		ini_parse_string_ctx ctx;
+
+		ctx.ptr = @string;
+		ctx.num_left = length;
+		return ini_parse_stream((ini_reader)ini_reader_string, ref ctx, handler,
+								user);
+	}
+}
---
+++ cs/ini_h.cs
@@ -0,0 +1,96 @@
+﻿/* inih -- simple .INI file parser
+
+SPDX-License-Identifier: BSD-3-Clause
+
+Copyright (C) 2009-2025, Ben Hoyt
+
+inih is released under the New BSD license (see LICENSE.txt). Go to the project
+home page for more info:
+
+https://github.com/benhoyt/inih
+
+*/
+
+/* Nonzero if ini_handler callback should accept lineno parameter. */
+#undef INI_HANDLER_LINENO
+
+using Crt;
+
+namespace inih;
+public static partial class ini
+{
+	/* Typedef for prototype of handler function.
+
+	   Note that even though the value parameter has type "const char*", the user
+	   may cast to "char*" and modify its content, as the value is not used again
+	   after the call to ini_handler. This is not true of section and name --
+	   those must not be modified.
+	*/
+#if INI_HANDLER_LINENO
+	typedef int (*ini_handler)(void* user, const char* section,
+                           const char* name, const char* value,
+                           int lineno);
+#else
+	public delegate int ini_handler(VoidPointer user, ConstPointer<char> section,
+						   ConstPointer<char> name, ConstPointer<char> value);
+#endif
+
+	/* Typedef for prototype of fgets-style reader function. */
+	public delegate Pointer<char> ini_reader(Pointer<char> str, int num, VoidPointer stream);
+
+
+	/* Parse given INI-style file. May have [section]s, name=value pairs
+	   (whitespace stripped), and comments starting with ';' (semicolon). Section
+	   is "" if name=value pair parsed before any section heading. name:value
+	   pairs are also supported as a concession to Python's configparser.
+
+	   For each name=value pair parsed, call handler function with given user
+	   pointer as well as section, name, and value (data only valid for duration
+	   of handler call). Handler should return nonzero on success, zero on error.
+
+	   Returns 0 on success, line number of first error on parse error (doesn't
+	   stop on first error), -1 on file open error, or -2 on memory allocation
+	   error (only when INI_USE_STACK is zero).
+	*/
+	public static partial int ini_parse(ConstPointer<char> filename, ini_handler handler, VoidPointer user);
+
+	/* Same as ini_parse(), but takes a FILE* instead of filename. This doesn't
+	   close the file when it's finished -- the caller must do that. */
+	public static partial int ini_parse_file(Pointer<FILE> file, ini_handler handler, VoidPointer user);
+
+	/* Same as ini_parse(), but takes an ini_reader function pointer instead of
+	   filename. Used for implementing custom or string-based I/O (see also
+	   ini_parse_string). */
+	public static partial int ini_parse_stream(ini_reader reader, VoidPointer stream, ini_handler handler,
+						 VoidPointer user);
+
+	/* Same as ini_parse(), but takes a zero-terminated string with the INI data
+	   instead of a file. Useful for parsing INI data from a network socket or
+	   which is already in memory. */
+	public static partial int ini_parse_string(ConstPointer<char> @string, ini_handler handler, VoidPointer user);
+
+	/* Same as ini_parse_string(), but takes a string and its length, avoiding
+	   strlen(). Useful for parsing INI data from a network socket or which is
+	   already in memory, or interfacing with C++ std::string_view. */
+	public static partial int ini_parse_string_length(ConstPointer<char> @string, size_t length, ini_handler handler, VoidPointer user);
+
+
+	/* Chars that begin a start-of-line comment. Per Python configparser, allow
+	   both ; and # comments at the start of a line by default. */
+	const string INI_START_COMMENT_PREFIXES = ";#";
+
+	/* Nonzero to allow inline comments (with valid inline comment characters
+	   specified by INI_INLINE_COMMENT_PREFIXES). Set to 0 to turn off and match
+	   Python 3.2+ configparser behaviour. */
+	const string INI_INLINE_COMMENT_PREFIXES = ";";
+
+	/* Maximum line length for any line in INI file (stack or heap). Note that
+	   this must be 3 more than the longest line (due to '\r', '\n', and '\0'). */
+	const size_t INI_MAX_LINE = (size_t)200;
+
+	/* Initial size in bytes for heap line buffer. Only applies if INI_USE_STACK
+	   is zero. */
+	const size_t INI_INITIAL_ALLOC = (size_t)200;
+}
+
+
